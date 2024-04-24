a3dc21510a35eb622db600aa22ef90ddbdd904c0
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2024-04-24 17:33:14 +0000

    fix crashes

--- src/util/proxyPinger.ts
+++ src/util/proxyPinger.ts
@@ -4,22 +4,26 @@ const nodejsTcpPing = require('nodejs-tcp-ping');
 import log from 'fancy-log';
 
 export async function ping(ip: string, port: number): Promise<number | null> {
-  log(`Pinging: ${ip}:${port}...`);
-  const result: tcpPingResultType[] = await nodejsTcpPing.tcpPing({
-    attempts: TCPConfig.attempts,
-    host: ip,
-    port,
-    timeout: TCPConfig.timeout,
-  } as tcpPingType);
+  try {
+    log(`Pinging: ${ip}:${port}...`);
+    const result: tcpPingResultType[] = await nodejsTcpPing.tcpPing({
+      attempts: TCPConfig.attempts,
+      host: ip,
+      port,
+      timeout: TCPConfig.timeout,
+    } as tcpPingType);
 
-  let pingData: number | null = null;
+    let pingData: number | null = null;
 
-  log('Ping results for', ip, port, result);
+    log('Ping results for', ip, port, result);
 
-  result.forEach((pingResult: tcpPingResultType) => {
-    if (pingResult.ping != null) {
-      return (pingData = Math.floor(pingResult.ping));
-    }
-  });
-  return pingData;
+    result.forEach((pingResult: tcpPingResultType) => {
+      if (pingResult.ping != null) {
+        return (pingData = Math.floor(pingResult.ping));
+      }
+    });
+    return pingData;
+  } catch (e: any) {
+    return null;
+  }
 }
